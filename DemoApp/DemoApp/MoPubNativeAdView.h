cb6f737b7b0c83e509cc5ff66079c4e800ed5923
Shimi Sheetrit <shimi.s@matomy.com>
2016-03-14 18:36:54 +0200

    MoPub Adapter changed

---
+++ DemoApp/DemoApp/MoPubNativeAdView.h
@@ -0,0 +1,13 @@
+//
+//  MoPubNativeAdView.h
+//  DemoApp
+//
+//  Created by Shimi Sheetrit on 3/14/16.
+//  Copyright © 2016 Matomy Media Group Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoPubNativeAdView : UIView
+
+@end
---
+++ DemoApp/DemoApp/MoPubNativeAdView.m
@@ -0,0 +1,21 @@
+//
+//  MoPubNativeAdView.m
+//  DemoApp
+//
+//  Created by Shimi Sheetrit on 3/14/16.
+//  Copyright © 2016 Matomy Media Group Ltd. All rights reserved.
+//
+
+#import "MoPubNativeAdView.h"
+
+@implementation MoPubNativeAdView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
