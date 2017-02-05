0c74a0d7f65b931e1bbb74943046aa96481551b3
Shimi Sheetrit <shimi.s@matomy.com>
2017-02-05 12:38:26 +0200

    MyMFSDKCore.podspec added

---
+++ MobFoxSDK.podspec.rb
@@ -0,0 +1,12 @@
+Pod::Spec.new do |s|
+  s.name = 'RestKit'
+
+  s.subspec 'Core' do |cs|
+    cs.dependency 'RestKit/ObjectMapping'
+    cs.dependency 'RestKit/Network'
+    cs.dependency 'RestKit/CoreData'
+  end
+
+  s.subspec 'ObjectMapping' do |os|
+  end
+end
